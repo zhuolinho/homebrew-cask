f1f916c88ff09efe8784152d4118b0e31763f496
zhuolinho <zhuolinho@hotmail.com>
2019-05-21 11:19:06 +0800

    Update cocoscreator.rb

--- Casks/cocoscreator.rb
+++ Casks/cocoscreator.rb
@@ -2,7 +2,7 @@ cask 'cocoscreator' do
   version '2.1.1_20190429'
   sha256 'efc20fb6bbad8a21a4c5f97f62a817c85155213419cc26f5ec43740452882967'
 
-  url "https://digitalocean.cocos2d-x.org/CocosCreator/v#{version.split('_')[0]}/CocosCreator_v#{version}_mac.dmg"
+  url "https://download.cocos.com/CocosCreator/v#{version.split('_')[0]}/CocosCreator_v#{version}_mac.dmg"
   appcast 'https://cocos2d-x.org/download'
   name 'CocosCreator'
   homepage 'https://cocos2d-x.org/'
